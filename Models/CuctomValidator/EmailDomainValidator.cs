13db908e463c09852d078d5b9885e9efe046b346
cahsul <cahsul.dev@gmail.com>
2021-04-26 05:12:56 +0700

    custom form validation

---
+++ Models/CuctomValidator/EmailDomainValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.CustomValidators
+{
+    public class EmailDomainValidator : ValidationAttribute
+    {
+        public string AllowedDomain { get; set; }
+
+        protected override ValidationResult IsValid(object value,
+            ValidationContext validationContext)
+        {
+            string[] strings = value.ToString().Split('@');
+            if (strings[1].ToUpper() == AllowedDomain.ToUpper())
+            {
+                return null;
+            }
+
+            return new ValidationResult($"Domain must be {AllowedDomain}",
+            new[] { validationContext.MemberName });
+        }
+    }
+}
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models.CustomValidators;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -14,6 +15,7 @@ namespace Models
         [Required]
         public string LastName { get; set; }
         [EmailAddress]
+        [EmailDomainValidator(AllowedDomain = "pragimtech.com")]
         public string Email { get; set; }
         public DateTime DateOfBrith { get; set; }
         public Gender Gender { get; set; }
