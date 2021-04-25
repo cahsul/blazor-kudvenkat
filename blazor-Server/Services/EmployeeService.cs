a9d37159dea23505daea8af0580a4ada619dbeab
cahsul <cahsul.dev@gmail.com>
2021-04-25 14:26:48 +0700

    Blazor route parameters

--- blazor-Server/Services/EmployeeService.cs
+++ blazor-Server/Services/EmployeeService.cs
@@ -21,5 +21,11 @@ namespace blazor_Server.Services
         {
             return await httpClient.GetJsonAsync<Employee[]>("api/employees");
         }
+
+        public async Task<Employee> GetEmployee(int id)
+        {
+            return await httpClient.GetJsonAsync<Employee>($"api/employees/{id}");
+        }
+
     }
 }
--- blazor-Server/Services/IEmployeeService.cs
+++ blazor-Server/Services/IEmployeeService.cs
@@ -9,5 +9,6 @@ namespace blazor_Server.Services
     public interface IEmployeeService
     {
         Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(int id);
     }
 }
