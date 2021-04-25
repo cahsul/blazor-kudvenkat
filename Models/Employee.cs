773fdba09d7159b0740ac875f4e4a760739ff469
cahsul <cahsul.dev@gmail.com>
2021-04-26 05:03:39 +0700

    form validation

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -13,6 +13,7 @@ namespace Models
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public DateTime DateOfBrith { get; set; }
         public Gender Gender { get; set; }
